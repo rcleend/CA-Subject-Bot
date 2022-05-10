db588ad7a20bcd95c4276ac9869263bbe8c1f028
Rcleend <rcleende@gmail.com>
2022-05-10 09:54:04 +0200

    Updated personality

--- src/main/kotlin/furhatos/app/ca_project/setting/personality.kt
+++ src/main/kotlin/furhatos/app/ca_project/setting/personality.kt
@@ -1,8 +1,7 @@
 package furhatos.app.ca_project.setting
 
 object Personality {
-    const val SENS_TO_FRIENDLINESS: Double = 0.1
-    const val SENS_TO_NEG_ASSUMPTION: Double = 0.1
-    const val SENS_TO_QUESTION_TYPE: Double = 0.05
+    const val SENS_TO_FRIENDLINESS: Double = 0.3
+    const val SENS_TO_NEG_ASSUMPTION: Double = 0.09
+    const val SENS_TO_QUESTION_TYPE: Double = 0.09
 }
-
